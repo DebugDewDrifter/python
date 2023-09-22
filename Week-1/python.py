45b779173b15ad53c0e728b7e9e56f31122aa4fb
Abhay Mourya <143886910+DebugDewDrifter@users.noreply.github.com>
2023-09-22 18:50:04 +0530

    My first python file

---
+++ Week-1/python.py
@@ -0,0 +1,23 @@
+# Print 
+print("Hello World")
+
+# + plus
+# - minus
+# / divide
+# * times
+# ** exponentiation
+# % modulo
+# < less then
+# > greater then
+
+value = 50
+print(value)
+
+sum = 2 + 5
+print(sum)
+v
+print("Is it true that 5 * 2 > 3 * 4?") 
+print(5 * 2 > 3 * 4)
+
+print("What is 5 * 2?", 5 * 2)
+print("What is 3 * 4?", 3 * 4)
