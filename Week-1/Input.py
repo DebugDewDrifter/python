fd8c71130b5b6f14b03a989206175322558b263c
Abhay Mourya <143886910+DebugDewDrifter@users.noreply.github.com>
2023-09-23 09:22:19 +0530

    Learned about Input

---
+++ Week-1/Input.py
@@ -0,0 +1,20 @@
+# Revision 
+# hehe hehe this is comment
+name = "Abhay Mourya"
+fee = 200 
+gst = 2.80 
+total = fee + gst 
+print(name ,"total money will be", total)
+
+# Input
+name = input("What's your name? ")
+age = int(input("How old are you? "))
+
+age_in_dog_years = age * 7
+
+country = input("Where you from? ")
+print(f"Ooh, You are from {country}")
+print(f"Hi {name}, it's cool that you're {age}. Don't worry.")
+
+
+print(f"In dog years you're {age_in_dog_years} years old.")
