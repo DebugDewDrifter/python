cf888c49823f9560e1428d280aadbec5608da366
Abhay Mourya <143886910+DebugDewDrifter@users.noreply.github.com>
2023-09-24 15:13:45 +0530

    If & else statement

---
+++ Week-1/if-else.py
@@ -0,0 +1,10 @@
+answer = input("do you want to hear a joke ")
+
+if answer == "Yes" or answer == "yes":
+ print("im against picketing, but I don't know how to show it.")
+elif answer == "No" or answer == "no":
+ print("Nan you rude")
+else:
+ print("What? i don't know what are you saying?")
+
+  
