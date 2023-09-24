db9e88c41a827814c9c99eb1d83ccbcb9303cd13
Abhay Mourya <143886910+DebugDewDrifter@users.noreply.github.com>
2023-09-24 15:21:04 +0530

    If else programs

--- Week-1/if-else-2.py
+++ Week-1/if-else-2.py
@@ -1,1 +1,22 @@
+#To check whether the year entered is leap
+year = int(input("Enter Year: "))
+if (year%4==0) and (year%100!=0):
+  print("{0} is leap year".format(year))
+else:
+  print("{0} is not leap year".format(year))
 
+
+# Odd Or Even Checker
+Number = int(input("Enter Number: "))
+if Number%2==0:
+  print(f"{Number} Is Even")
+else:
+  print(f"{Number} Is Odd")
+
+
+# Prime Number Checker
+Number = int(input("Enter Number: "))
+if Number%Number==0 and Number%2!=0:
+  print(f"{Number} Is Prime Number")
+else:
+  print(f"{Number} Is Not A Prime Number")
